7255a0e11d482df2818dabaf359c360dbc1be1ba
rtm516 <ryantmilner@hotmail.co.uk>
2020-10-26 23:07:59 +0000

    Remove categories array and add clothes type checking

--- src/converter.py
+++ src/converter.py
@@ -12,30 +12,13 @@ precursor = """<CurrencyName> #tm_ruble
 	<Currency> MoneyRuble50, 	50
 	<Currency> MoneyRuble100, 	100\n"""
 
-categories = {
-	"Food": [],
-	"Containers": [],
-	"Tools": [],
-	"Weapons": [],
-	"Explosives": [],
-	"Clothes": [],
-	"Vehiclesparts": [],
-	"Vehicles": [],
-	"Viking": [],
-	"Bbp": [],
-	"Msp": [],
-	"Mass": [],
-	"Cp": [],
-	"Mung": [],
-	"Ammo": [],
-	"Mag": []
-}
+categories = {}
 
 traders = {
 	"Consume Trader": ["Food"], # 0
 	"Misc Trader": ["Containers", "Tools", "Viking", "Bbp", "Msp", "Mass", "Cp", "Mung"], # 1
 	"Weapon Trader": ["Weapons", "Explosives"], # 2
-	"Clothing Trader": ["Clothes"], # 3
+	"Clothing Trader": ["Shirts", "Pants", "Coats", "Jackets", "Vests", "Bags", "Shoes", "Boots", "Belts", "Helmet", "Glasses", "Armbands", "Ghillie", "Other Clothes"], # 3
 	"Weapon Supplies Trader": ["Mag", "Ammo"], # 4
 	"Vehicles Trader": ["Vehiclesparts", "Vehicles"] # 5
 }
@@ -76,6 +59,49 @@ for child in root:
 	if name[:7].lower() == "skyline":
 		continue
 
+	elif category == "clothes":
+		if ("shirt" in name.lower()):
+			category = "Shirts"
+
+		elif ("pant" in name.lower()):
+			category = "Pants"
+
+		elif ("coat" in name.lower()):
+			category = "Coats"
+
+		elif ("jacket" in name.lower()):
+			category = "Jackets"
+
+		elif ("vest" in name.lower() or "platecarrier" in name.lower()):
+			category = "Vests"
+
+		elif ("bag" in name.lower()):
+			category = "Bags"
+
+		elif ("shoe" in name.lower()):
+			category = "Shoes"
+
+		elif ("boot" in name.lower()):
+			category = "Boots"
+
+		elif ("belt" in name.lower()):
+			category = "Belts"
+
+		elif ("helmet" in name.lower()):
+			category = "Helmets"
+
+		elif ("glasses" in name.lower()):
+			category = "Glasses"
+
+		elif ("armband" in name.lower()):
+			category = "Armbands"
+
+		elif ("ghillie" in name.lower()):
+			category = "Ghillie"
+			
+		else:
+			category = "Other Clothes"
+
 	elif name[:3].lower() == "bbp":
 		category = "Bbp"
 	
@@ -110,12 +136,12 @@ for child in root:
 		category = "Vehicles"
 		quantity = "V"
 
+	if not (category.capitalize() in categories):
+		categories[category.capitalize()] = []
+
 	categories[category.capitalize()].append({"name": name, "quantity": quantity})
 
-if os.path.exists("TraderConfig.txt"): 
-	os.remove("TraderConfig.txt")
-
-f = open("TraderConfig.txt", "a+")
+f = open("TraderConfig.txt", "w")
 f.write(precursor)
 
 for trader in traders:
