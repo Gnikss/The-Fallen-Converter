c0e581cecbdcf8529709faeb76b4494507b995e3
Gnikss <dpantovich123@gmail.com>
2020-10-26 23:00:41 -0400

    Updating converter.py
    Added more categories to traders diversifying the trader list

--- src/converter.py
+++ src/converter.py
@@ -15,11 +15,11 @@ precursor = """<CurrencyName> #tm_ruble
 categories = {}
 
 traders = {
-	"Consume Trader": ["Food"], # 0
-	"Misc Trader": ["Containers", "Tools", "Viking", "Bbp", "Msp", "Mass", "Cp", "Mung"], # 1
+	"Consume Trader": ["Food", "Canteens", "Seeds"], # 0
+	"Misc Trader": ["Containers", "Tools", "Kits", "Viking", "Bbp", "Msp", "Mass", "Cp", "Mung"], # 1
 	"Weapon Trader": ["Weapons", "Explosives"], # 2
-	"Clothing Trader": ["Shirts", "Pants", "Coats", "Jackets", "Vests", "Bags", "Shoes", "Boots", "Belts", "Helmet", "Glasses", "Armbands", "Ghillie", "Other Clothes"], # 3
-	"Weapon Supplies Trader": ["Mag", "Ammo"], # 4
+	"Clothing Trader": ["Shirts", "Pants", "Coats", "Jackets", "Vests", "Bags", "Shoes", "Gear", "Boots", "Belts", "Helmet", "Masks", "Glasses", "Armbands", "Ghillie", "OtherClothes"], # 3
+	"Weapon Supplies Trader": ["Mags", "Ammo", "Attachments", "Optics"], # 4
 	"Vehicles Trader": ["Vehiclesparts", "Vehicles"] # 5
 }
 
@@ -59,48 +59,47 @@ for child in root:
 	if name[:7].lower() == "skyline":
 		continue
 
-	elif category == "clothes":
-		if ("shirt" in name.lower()):
-			category = "Shirts"
+	elif ("shirt" in name.lower()):
+		category = "Shirts"
 
-		elif ("pant" in name.lower()):
-			category = "Pants"
+	elif ("pant" in name.lower()):
+		category = "Pants"
 
-		elif ("coat" in name.lower()):
-			category = "Coats"
+	elif ("coat" in name.lower()):
+		category = "Coats"
 
-		elif ("jacket" in name.lower()):
-			category = "Jackets"
+	elif ("jacket" in name.lower()):
+		category = "Jackets"
 
-		elif ("vest" in name.lower() or "platecarrier" in name.lower()):
-			category = "Vests"
+	elif ("vest" in name.lower() or "platecarrier" in name.lower()):
+		category = "Vests"
 
-		elif ("bag" in name.lower()):
-			category = "Bags"
+	elif ("pouch" in name.lower() or "holster" in name.lower() or "sheath" in name.lower()):
+		category = "Gear"
 
-		elif ("shoe" in name.lower()):
-			category = "Shoes"
+	elif ("shoe" in name.lower()):
+		category = "Shoes"
 
-		elif ("boot" in name.lower()):
-			category = "Boots"
+	elif ("boot" in name.lower()):
+		category = "Boots"
 
-		elif ("belt" in name.lower()):
-			category = "Belts"
+	elif ("belt" in name.lower()):
+		category = "Belts"
 
-		elif ("helmet" in name.lower()):
-			category = "Helmets"
+	elif ("helmet" in name.lower()):
+		category = "Helmets"
 
-		elif ("glasses" in name.lower()):
-			category = "Glasses"
+	elif ("glasses" in name.lower()):
+		category = "Glasses"
 
-		elif ("armband" in name.lower()):
-			category = "Armbands"
+	elif ("armband" in name.lower() or "patch" in name.lower()):
+		category = "Armbands"
 
-		elif ("ghillie" in name.lower()):
-			category = "Ghillie"
-			
-		else:
-			category = "Other Clothes"
+	elif ("ghillie" in name.lower()):
+		category = "Ghillie"
+
+	elif ("mask" in name.lower() or "respirator" in name.lower() or "shemagh" in name.lower()):
+		category = "Masks"
 
 	elif name[:3].lower() == "bbp":
 		category = "Bbp"
@@ -120,11 +119,32 @@ for child in root:
 	elif name[:11].lower() == "rag_hummer_":
 		category = "Vehiclesparts"
 
-	elif name[:4].lower() == "mag_":
-		category = "Mag"
+	elif name[-4:].lower() == "_kit":
+		category = "Kits"
 
-	elif name[:4].lower() == "ammo":
+	elif ("bag" in name.lower()):
+			category = "Bags"
+
+	elif ("fridge" in name.lower()):
+		category = "Containers"
+
+	elif ("canteen" in name.lower()):
+		category = "Canteens"
+
+	elif ("seed" in name.lower()):
+		category = "Seeds"
+
+	elif ("ammo" in name.lower()):
 		category = "Ammo"
+
+	elif ("mag" in name.lower()):
+		category = "Mags"
+
+	elif ("bttstck" in name.lower() or "compensator" in name.lower() or "hndgrd" in name.lower() or "hndguard" in name.lower() or "buttstock" in name.lower() or "suppressor" in name.lower() or "mount" in name.lower()):
+		category = "Attachments"
+
+	elif ("optic" in name.lower() or "acog" in name.lower() or "sight" in name.lower() or "scope" in name.lower()):
+		category = "Optics"
 
 	elif name[-9:].lower() == "destroyed":
 		continue
